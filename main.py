0fe68cc675b7692ba4cba2dac145fa6d4b463553
Manoj <contact.manojkrishna@gmail.com>
2026-08-26 07:34:08 +0530

    Valid AI chaoice

--- main.py
+++ main.py
@@ -11,3 +11,24 @@ def get_number(message):
             print("Enter a number from 1 to 6.")
         except ValueError:
             print("Enter a valid number.")
+
+
+def ai_choice(history, batting):
+    if len(history) < 3:
+        return random.randint(1, 6)
+
+    count = Counter(history)
+
+    if batting:
+        weights = []
+
+        for n in range(1, 7):
+            weights.append(max(1, 10 - count[n] * 2))
+
+    else:
+        weights = []
+
+        for n in range(1, 7):
+            weights.append(1 + count[n] * 2)
+
+    return random.choices(range(1, 7), weights=weights)[0]
